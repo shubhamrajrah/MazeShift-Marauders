d4bf466b67264a30576deb66eee4f528c04443a7
Kanishk Shah <shahkanishk99@gmail.com>
2023-11-28 14:10:07 -0800

    Update Player Controls for Level 7 to work with Star panel

--- Assets/Scripts/PlayerControlsLevel7.cs
+++ Assets/Scripts/PlayerControlsLevel7.cs
@@ -229,6 +229,27 @@ public class PlayerControlsLevel7 : MonoBehaviour
             collision.gameObject.SetActive(false);
             _levelInfo.KeyCollected++;
             keyText.text = string.Format(_keyTextFormat, _keyGet, keyNum);
+
+            Transform starsInGamePlayTransform = GameObject.Find("StarsInGamePlay").transform;
+            Transform goldStarsTransform = starsInGamePlayTransform.Find("GoldStarsInGamePlay");
+
+            if (goldStarsTransform != null)
+            {
+                string goldStarName = "GoldStarGP" + _keyGet;
+                Transform goldStarChild = goldStarsTransform.Find(goldStarName);
+                if (goldStarChild != null)
+                {
+                    goldStarChild.gameObject.SetActive(true);
+                }
+                else
+                {
+                    Debug.LogWarning("GoldStar child with name " + goldStarName + " not found in starsInGamePlay!");
+                }
+            }
+            else
+            {
+                Debug.LogWarning("goldStarsInGamePlay not found in starsInGamePlay!");
+            }
             if (_keyGet == keyNum && !_isDescending)
             {
                 endBlock.GetComponent<Renderer>().material.color = targetFinish;
